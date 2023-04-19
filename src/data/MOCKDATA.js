9738599806f9ffa51d8ee3a9bbcfbf1796d367b2
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2023-04-19 03:16:23 -0500

    feat: tags (languages) for projects

--- src/data/MOCKDATA.js
+++ src/data/MOCKDATA.js
@@ -2,8 +2,13 @@ const allProjects = [
   {
     id: "01",
     title: "Portafolio popehiflo.",
-    image: "./public/assets/imgs/img-frontend-0.jpg",
+    image: "./public/assets/imgs/img-portfolio.png",
     description: "Portafolio construido con HTML, CSS y Javascript",
+    tags: {
+      html: "html",
+      css: "css",
+      js: "js"
+    },
     category: {
       id: "frontend",
       name: "Frontend"
@@ -19,6 +24,9 @@ const allProjects = [
     title: "Proyecto Backend 02",
     image: "./public/assets/imgs/img-backend-0.png",
     description: "2 - Proyecto de una API con Node y Express. Este es un proyecto backend.",
+    tags: {
+      java: "java"
+    },
     category: {
       id: "backend",
       name: "Backend"
@@ -34,6 +42,11 @@ const allProjects = [
     title: "Proyecto Frontend 03",
     image: "./public/assets/imgs/img-frontend-0.jpg",
     description: "3- Proyecto de sitio web estático en HTML + CSS. Este es un proyecto frontend.",
+    tags: {
+      vite: "vite",
+      reactjs: "reactjs",
+      ts: "ts"
+    },
     category: {
       id: "frontend",
       nombre: "Frontend"
@@ -49,6 +62,9 @@ const allProjects = [
     title: "Proyecto Backend 04 con un nombre un poco mas largo",
     image: "./public/assets/imgs/img-backend-1.png",
     description: "4 - Proyecto de una API con Node y Express. Este es un proyecto backend.",
+    tags: {
+      nodejs: "nodejs"
+    },
     category: {
       id: "backend",
       name: "Backend"
@@ -64,6 +80,10 @@ const allProjects = [
     title: "Proyecto Frontend 05",
     image: "./public/assets/imgs/img-frontend-0.jpg",
     description: "5 - Proyecto de sitio web estático en HTML + CSS. Este es un proyecto frontend.",
+    tags: {
+      angular: "angular",
+      rxjs: "rxjs"
+    },
     category: {
       id: "frontend",
       nombre: "Frontend"
@@ -79,6 +99,12 @@ const allProjects = [
     title: "Proyecto Backend 06",
     image: "./public/assets/imgs/img-backend-2.png",
     description: "6 - Proyecto de una API con Spring Boot y MongoDB. Este es un proyecto backend.",
+    tags: {
+      java: "java",
+      springboot: "springboot",
+      jpa: "jpa",
+      mysql: "mysql"
+    },
     category: {
       id: "backend",
       name: "Backend"
